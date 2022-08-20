260afeb58b0c1fd0c9c785e9b5d9a4c3f2dbcd57
AzizaJonathan <azizajonathan74@gmail.com>
2022-08-20 17:03:42 +0200

    Item List Display

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+
+$filename = __DIR__ . '/data/articles.json';
+$articles = [];
+
+if (file_exists($filename)) {
+    $articles = json_decode(file_get_contents($filename), true) ?? [];
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,7 +23,16 @@
     <div class="container">
         <?php require_once 'includes/header.php' ?>
         <div class="content">
-
+            <div class="articles-container">
+                <?php foreach ($articles as $a) : ?>
+                    <div class="article block">
+                        <div class="overflow">
+                            <div class="img-container" style="background-image:url(<?= $a['image'] ?>"></div>
+                        </div>
+                        <h2><?= $a['title'] ?></h2>
+                    </div>
+                <?php endforeach; ?>
+            </div>
         </div>
         <?php require_once 'includes/footer.php' ?>
     </div>
