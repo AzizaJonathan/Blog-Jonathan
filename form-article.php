680631245acf9cfac745868388b578e708bd610c
AzizaJonathan <azizajonathan74@gmail.com>
2022-09-12 15:12:10 +0200

    Setting up the logic for registration

--- form-article.php
+++ form-article.php
@@ -88,7 +88,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <head>
     <?php require_once 'includes/head.php' ?>
-    <link rel="stylesheet" href="/public/css/form-article.css">
+    <!--<link rel="stylesheet" href="/public/css/form-article.css">-->
     <title><?= $id ? 'Modifier' : 'Créer' ?> un article</title>
 </head>
 
--- includes/header.php
+++ includes/header.php
@@ -4,5 +4,21 @@
         <li class=<?= $_SERVER['REQUEST_URI'] === '/form-article.php' ? 'active' : '' ?>>
             <a href=" /form-article.php">Ecrire un article</a>
         </li>
+        <li class=<?= $_SERVER['REQUEST_URI'] === '/auth-article.php' ? 'active' : '' ?>>
+            <a href=" /auth-register.php">Inscription</a>
+        </li>
+        <li class=<?= $_SERVER['REQUEST_URI'] === '/form-login.php' ? 'active' : '' ?>>
+            <a href=" /auth-login.php">Connexion</a>
+        </li>
+        <li>
+            <a href=" /auth-logout.php">Déconnexion</a>
+        </li>
+        <li class=<?= $_SERVER['REQUEST_URI'] === '/profil.php' ? 'active' : '' ?>>
+            <a href=" /profil.php">Ma page</a>
+        </li>
+
+
+
+
     </ul>
 </header>
